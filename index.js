e19a6d0f696619e360963e4eee47757d37eff492
mandeeep03 <mandeepbhatt52@gmail.com>
2025-12-27 18:30:05 +0530

    incomplete

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express')
 const routerUrl = require('./Router/url')
 const {connectToMonDb} = require('./connect')
+const URL = require('./Models/url')
 
 const app = express()
 app.use(express.json())
 const PORT = 8001
 connectToMonDb('mongodb://127.0.0.1:27017/short-url')
 app.use('/url',routerUrl)
+
+app.get('/:shortId',async (req,res)=>{
+    const shortId = req.params.shortId
+    const entry =  await URL.findOneAndUpdate({
+        shortId
+    },{
+        $push:{
+            visitHistory:{
+                timeStamp : Date.now()
+            }
+        }
+    })
+    res.redirect(entry.redirectUrl)
+
+})
+
 
 app.listen(PORT,()=>{
     console.log(`Serever Running on http://localhost:${PORT}/`);
