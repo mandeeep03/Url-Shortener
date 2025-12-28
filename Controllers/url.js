46c2f19a533dd81d958f8effb3c20661a1b623ea
mandeeep03 <mandeepbhatt52@gmail.com>
2025-12-28 20:24:40 +0530

    done

--- Controllers/url.js
+++ Controllers/url.js
@@ -17,7 +17,7 @@ async function handleGetAnalytics(req,res){
     const shortId = req.params.shortId
     const result = await URL.findOne({shortId})
     if (!result) {
-      return res.status(404).json({ error: "Short URL not found" });
+        return res.status(404).json({ error: "Short URL not found" });
     }
     return res.json({totalClicks:result.visitHistory.length,analytics:result.visitHistory})
 }
