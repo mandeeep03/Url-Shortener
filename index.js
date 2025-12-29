f9196398ba3c1aa6b6d644db68eb769df6a5b978
mandeeep03 <mandeepbhatt52@gmail.com>
2025-12-29 20:09:06 +0530

    done genrating url in ejs

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ const express = require('express')
 const routerUrl = require('./Router/url')
 const {connectToMonDb} = require('./connect')
 const URL = require('./Models/url')
+const Path = require('path')
+const staticRouter = require('./Router/staticRouter')
 
 const app = express()
 app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
 const PORT = 8001
 connectToMonDb('mongodb://127.0.0.1:27017/short-url')
-app.use('/url',routerUrl)
 
-app.get('/:shortId',async (req,res)=>{
+app.use('/url',routerUrl)
+app.set('view engine','ejs')
+app.set('views',Path.resolve('./Views'))
+app.use('/',staticRouter)
+
+app.get('/url/:shortId',async (req,res)=>{
     const shortId = req.params.shortId
     const entry =  await URL.findOneAndUpdate({
         shortId
